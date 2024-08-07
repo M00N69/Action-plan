41bb0e46aadc50f9d60df7a4fcabbe7ec1b461d7
MOON <133333114+M00N69@users.noreply.github.com>
2024-08-07 14:44:02 +0200

    Update action_plan_app.py

--- action_plan_app.py
+++ action_plan_app.py
@@ -4,8 +4,8 @@ import requests
 import google.generativeai as genai
 
 # Load VISIPACT data and IFS checklist from GitHub
-visipact_df = pd.read_csv("https://raw.githubusercontent.com/your-username/your-repo-name/main/data/output%20vsipact.csv")
-ifs_checklist_df = pd.read_csv("https://raw.githubusercontent.com/your-username/your-repo-name/main/ifs/Guide%20Checklist_IFS%20Food%20V%208%20-%20CHECKLIST.csv")
+visipact_df = pd.read_csv("https://raw.githubusercontent.com/M00N69/Action-plan/main/data/output%20vsipact.csv")  # Raw content URL
+ifs_checklist_df = pd.read_csv("https://raw.githubusercontent.com/M00N69/Action-plan/main/ifs/Guide%20Checklist_IFS%20Food%20V%208%20-%20CHECKLIST.csv")
 
 # Function to load the user-uploaded action plan
 def load_action_plan(uploaded_file):
