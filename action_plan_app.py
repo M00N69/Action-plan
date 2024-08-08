ba627beabae7f687a94477b734e316d4b584c47c
MOON <133333114+M00N69@users.noreply.github.com>
2024-08-08 12:13:36 +0200

    Update action_plan_app.py

--- action_plan_app.py
+++ action_plan_app.py
@@ -218,6 +218,7 @@ def main():
                         st.write("Bouton cliqué")  # Ajout de message de débogage
                         st.session_state.action_plan_df = action_plan_df
                         st.session_state.page = 2
+                        st.experimental_rerun()
 
     if st.session_state.page == 2:
         st.write("Chargement de la page 2")  # Ajout de message de débogage
